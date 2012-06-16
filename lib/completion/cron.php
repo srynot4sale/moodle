4f7c809a1ecc374741342793ed92847867deed34
Aaron Barnes <aaronb@catalyst.net.nz>
2012-06-16 22:47:48 +1200

    MDL-33788 completion: SQL optimisation

--- lib/completion/cron.php
+++ lib/completion/cron.php
@@ -383,6 +383,7 @@ function completion_cron_completions() {
             reaggregate = 0
         WHERE
             reaggregate < {$timestarted}
+        AND reaggregate > 0
     ";
 
     $DB->execute($sql);

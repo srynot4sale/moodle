b6149d7ad0e645da4fe4c93a5303213e42431b9c
Dan Marsden <dan@danmarsden.com>
2014-10-17 12:50:00 +1300

    Make sure grade header appears when advanced grading enabled.

--- course/moodleform_mod.php
+++ course/moodleform_mod.php
@@ -173,7 +173,7 @@ abstract class moodleform_mod extends moodleform {
                     // TODO: add a "Mixed categories" text instead of removing elements with no explanation
                     if ($mform->elementExists('gradecat')) {
                         $mform->removeElement('gradecat');
-                        if ($this->_features->rating) {
+                        if ($this->_features->rating && !$this->_features->advancedgrading) {
                             //if supports ratings then the max grade dropdown wasnt added so the grade box can be removed entirely
                             $mform->removeElement('modstandardgrade');
                         }
